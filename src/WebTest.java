3c345c40e3e60dc92b9737debaa62446c4dd7960
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2013-01-14 18:03:58 -0800

    playing around with the spark web framework

---
+++ src/WebTest.java
@@ -0,0 +1,14 @@
+import static spark.Spark.*;
+import spark.*;
+
+public class WebTest {
+
+    public static void main(String[] args) {
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!";
+            }
+        });
+    }
+}
