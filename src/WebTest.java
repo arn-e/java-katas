b222bad2d9bb931ddfa8c8f7da1c8d37b5e52a78
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2013-01-15 11:12:53 -0800

    fixed bug in web test, static html page returned on root request

--- src/WebTest.java
+++ src/WebTest.java
@@ -1,36 +1,33 @@
 import static spark.Spark.*;
 import spark.*;
-
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebTest {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         setPort(9000);
-        File newFile = new File("/Users/arne/IdeaProjects/testEnvironment/public/index.html");
-        FileInputStream fis1 = new FileInputStream(newFile);
-        FileReader input = new FileReader(newFile);
-        final BufferedReader buffer = new BufferedReader(input);
+        final String filename = "./public/index.html";
 
         get(new Route("/") {
             @Override
             public Object handle(Request request, Response response){
-                String returnVal = "";
-                try {
-                    returnVal = buffer.readLine();
-                }
-                catch(IOException e) {
-                    e.printStackTrace();
-                }
-                return returnVal;
+                List<String> returnVal = new ArrayList<String>();
+
+                try { returnVal = Files.readAllLines(Paths.get(filename), Charset.defaultCharset()); }
+                catch(IOException e) { e.printStackTrace(); }
+
+                response.type("text/html");
+                StringBuilder cleaned = new StringBuilder();
+                for (String i : returnVal) { cleaned.append(i); }
+                return cleaned;
             }
         });
 
-        get(new Route("/hello") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return "Hello World!";
-            }
-        });
+
     }
 }
